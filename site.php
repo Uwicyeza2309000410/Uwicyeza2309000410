2bf81ee42e066288681333c01fd39886b9628ee7
Uwicyeza2309000410 <uwicyezasamilla0@gmail.com>
2025-04-13 09:14:52 +0200

    Add files via upload

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head><title>hello world</title></head>
+    <body>
+        <?php
+         echo"<p>hello there</p>";
+         echo"<p>welcome to our website</p>";
+        $name="Samilla Uwicyeza";
+        $Regnumber="2309000410";
+        $module="webdesign";
+        echo"<h2> personal detail</h2>";
+        echo"<p>my name is:$name</p>";
+        echo"<p>my reg number is:$Regnumber</p>";
+        echo"<p>the module is:$module</p>";
+
+
+        ?>
+    </body>
